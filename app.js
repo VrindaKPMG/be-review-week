f4015bbefe03b25976af98d7994dca1b3f72ca23
VrindaKPMG <vrinda.gera@kpmg.co.uk>
2022-12-15 10:09:09 +0000

    created a con-errors file based on review feedback

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const {getTopics, getArticles, getArticleById, getArticleCommentById, postCommentByArticleId} = require('./controllers/con-topics')
+const {handle404, handleOtherErrors, handle500} = require('./controllers/con-errors')
 
 app.use(express.json());
 
@@ -11,29 +12,13 @@ app.get('/api/articles/:article_id/comments', getArticleCommentById)
 app.post('/api/articles/:article_id/comments', postCommentByArticleId)
 
 //handles all incorrect path errors
-app.all('*', (req, res, next) => {
-    res.status(404).send({msg: 'Path not found :( Try again.'})
-});
+app.all('*', handle404)
 
-
-app.use((err, req, res, next) => {
-    if (err.code === '22P02' || err.code === '23502') {
-        res.status(400).send({msg : 'wrong request'})
-    }
-    if (err.code === '23503') {
-        res.status(404).send({msg : 'not found'})
-    }
-    if (err.msg && err.status) {
-        res.status(err.status).send({msg: err.msg})
-    }
-})
-
-
+//handles any other errors
+app.use(handleOtherErrors)
 
 //handles anything not specifically handled
-app.all('*', (err, req, res, next) => {
-    res.status(500).send({msg : 'something is wrong'})
-});
+app.all('*', handle500)
 
 
 
